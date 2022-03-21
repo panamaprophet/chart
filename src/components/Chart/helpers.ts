32432319cd2502edfdf03257b0e5911398d793b2
pp <pp@rawsound.ru>
2022-03-21 18:01:19 +0100

    line smoothness threshold

---
+++ src/components/Chart/helpers.ts
@@ -0,0 +1,43 @@
+export type ChartCoordinate = [x: number, y: number];
+
+
+export const drawSmoothLine = (context: CanvasRenderingContext2D, coordinates: ChartCoordinate[], color: string) => {
+    context.beginPath();
+    context.moveTo(coordinates[0][0], coordinates[0][1]);
+
+    for (let i = 0; i < coordinates.length - 1; i ++) {
+        const midX = (coordinates[i][0] + coordinates[i + 1][0]) / 2;
+        const midY = (coordinates[i][1] + coordinates[i + 1][1]) / 2;
+        const controlPointX1 = (midX + coordinates[i][0]) / 2;
+        const controlPointX2 = (midX + coordinates[i + 1][0]) / 2;
+
+        context.quadraticCurveTo(
+            controlPointX1,
+            coordinates[i][1],
+            midX,
+            midY,
+        );
+
+        context.quadraticCurveTo(
+            controlPointX2,
+            coordinates[i + 1][1],
+            coordinates[i + 1][0],
+            coordinates[i + 1][1],
+        );
+    }
+
+    context.strokeStyle = color;
+    context.stroke();
+}
+
+export const drawPolyLine = (context: CanvasRenderingContext2D, coordinates: ChartCoordinate[], color: string) => {
+    context.beginPath();
+    context.moveTo(coordinates[0][0], coordinates[0][1]);
+
+    for (let i = 1; i < coordinates.length; i ++) {
+        context.lineTo(coordinates[i][0], coordinates[i][1]);
+    }
+
+    context.strokeStyle = color;
+    context.stroke();
+}
--- src/components/Chart/index.tsx
+++ src/components/Chart/index.tsx
@@ -1,8 +1,7 @@
 import { useRef, useEffect, useState } from 'react';
 import { Zoom } from '../Zoom/index';
+import { drawPolyLine, drawSmoothLine, ChartCoordinate } from './helpers';
 import styles from './styles.module.css';
-
-type ChartCoordinate = [x: number, y: number];
 
 interface Props {
     labels: Record<string, string>,
@@ -11,10 +10,19 @@ interface Props {
     height?: number,
     lineWidth?: number,
     colors?: Record<string, string>,
+    smoothnessThreshold: number,
 }
 
 
-export const Chart = ({ labels, lines, colors = {}, width = 600, height = 300, lineWidth = 2 }: Props) => {
+export const Chart = ({
+    labels,
+    lines,
+    colors = {},
+    width = 600,
+    height = 300,
+    lineWidth = 2,
+    smoothnessThreshold = 50,
+}: Props) => {
     const colorsMap: Record<string, string> = {
         ...({ default: '#333333' }),
         ...colors,
@@ -24,55 +32,31 @@ export const Chart = ({ labels, lines, colors = {}, width = 600, height = 300, l
     const [bounds, setBounds] = useState([0, 100]);
 
     useEffect(() => {
-        if (!canvas.current) {
+        const context = canvas.current?.getContext('2d');
+
+        if (!canvas.current || !context) {
             return;
         }
 
-        const context = canvas.current.getContext('2d');
-
-        if (!context) {
-            return;
-        }
+        context.clearRect(0, 0, width, height);
 
         for (let key of Object.keys(lines)) {
             const boundStep = (lines[key].length / 100);
-            const values = lines[key].slice(
-                Math.round(bounds[0] * boundStep),
-                Math.round(bounds[1] * boundStep)
-            );
+            const values = lines[key].slice(Math.round(bounds[0] * boundStep), Math.round(bounds[1] * boundStep));
 
             const max = values.reduce((m, i) => Math.max(m, i), values[0]);
             const step = canvas.current.width / values.length;
             const verticalMultiplier = (canvas.current.height - lineWidth) / max;
-            const coordinates = values.map<ChartCoordinate>((value, index) => [index * step, value * verticalMultiplier]);
+            const coordinates = values.map<ChartCoordinate>((value, index) => [
+                index * step,
+                value * verticalMultiplier
+            ]);
 
-            context.clearRect(0, 0, width, height);
-            context.beginPath();
-            context.moveTo((coordinates[0][0]), coordinates[0][1]);
-
-            for(let i = 0; i < coordinates.length-1; i ++) {
-                const midX = (coordinates[i][0] + coordinates[i + 1][0]) / 2;
-                const midY = (coordinates[i][1] + coordinates[i + 1][1]) / 2;
-                const controlPointX1 = (midX + coordinates[i][0]) / 2;
-                const controlPointX2 = (midX + coordinates[i + 1][0]) / 2;
-
-                context.quadraticCurveTo(
-                    controlPointX1,
-                    coordinates[i][1],
-                    midX,
-                    midY,
-                );
-
-                context.quadraticCurveTo(
-                    controlPointX2,
-                    coordinates[i + 1][1],
-                    coordinates[i + 1][0],
-                    coordinates[i + 1][1],
-                );
+            if (coordinates.length > smoothnessThreshold) {
+                drawPolyLine(context, coordinates, colorsMap[key] || colorsMap['default']);
+            } else {
+                drawSmoothLine(context, coordinates, colorsMap[key] || colorsMap['default']);
             }
-
-            context.strokeStyle = colorsMap[key] || colorsMap['default']
-            context.stroke();
         }
     }, [canvas.current, bounds]);
 
