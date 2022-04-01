a14263034913bbabdf352a4ea370c8fd592997a6
pp <pp@rawsound.ru>
2022-04-01 12:06:33 +0200

    label positions in px

--- src/hooks/useChart/index.tsx
+++ src/hooks/useChart/index.tsx
@@ -17,7 +17,7 @@ export const useChart = (values: number[][], labels: (string | number)[], { widt
     } = useCoordinates(values, { width: width * pixelRatio, height: height * pixelRatio });
 
     const yLabels = useMemo(() => getAxisYLabels(0, max), [max]);
-    const yLabelsCoordinates = useMemo(() => yLabels.map((_, index, items) => `${(index / items.length) * 100}%`), [yLabels]);
+    const yLabelsCoordinates = useMemo(() => yLabels.map((_, index, items) => `${index * (width * pixelRatio / items.length)}px`), [yLabels]);
 
     const xLabels = useMemo(() => labels.slice(startIndex, endIndex), [startIndex, endIndex]);
     const xLabelsCoordinates = useMemo(() => xLabels.map((_, index) => `${coordinates[0][index][0] / pixelRatio}px`), [xLabels, coordinates]);
